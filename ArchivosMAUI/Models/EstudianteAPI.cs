30fdb7499523532afc6d1a7eb5906a81ced26f5b
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-11-28 09:03:47 -0500

    Apis Repository

---
+++ ArchivosMAUI/Models/EstudianteAPI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosMAUI.Models
+{
+    public class EstudianteAPI
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+        public Address address { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public List<string> courses { get; set; }
+        public double gpa { get; set; }
+        public string image { get; set; }
+    }
+    public class Address
+        {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string zip { get; set; }
+        public string country { get; set; }
+    }
+
+        
+           
+
+    
+}
---
+++ ArchivosMAUI/Repositories/EstudianteUDLAAPisRepository.cs
@@ -0,0 +1,57 @@
+﻿using ArchivosMAUI.Interfaces;
+using ArchivosMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchivosMAUI.Repositories
+{
+    public class EstudianteUDLAAPisRepository : IEstudianteUDLARepository
+
+    {
+
+        public string urlEndpoint = "https://www.freetestapi.com/api/v1/students ";
+        public HttpClient _httpClient;
+        public EstudianteUDLAAPisRepository()
+        {
+            _httpClient = new HttpClient();
+        }
+        public bool ActualizarEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CrearEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EstudianteUDLA DevuelveInfoEstudiante(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<EstudianteUDLA>> DevuelveListadoEstudiantesUDLA()
+        {
+            List<EstudianteUDLA> estudiantesUDLA = new List<EstudianteUDLA>();
+            try
+            {
+                using (_httpClient)
+                {
+                    var respone = await _httpClient.GetAsync(urlEndpoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public bool EliminarEstudianteUDLA(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
